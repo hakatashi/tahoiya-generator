e5c954c5db81f3219fde581350e107e58ccc8a57
Koki Takahashi <hakatasiloving@gmail.com>
2018-09-13 10:18:10 +0900

    Setup ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {}
-}
+	root: true,
+	parserOptions: {
+	  parser: 'babel-eslint',
+	},
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: [
+		'@hakatashi',
+		'plugin:vue/recommended',
+	],
+	// required to lint *.vue files
+	plugins: [
+		'vue',
+	],
+	// add your custom rules here
+	rules: {
+		'vue/html-indent': ['error', 'tab'],
+		'vue/mustache-interpolation-spacing': ['error', 'never'],
+		'vue/max-attributes-per-line': ['error', {
+			singleline: 3,
+			multiline: {
+				max: 1,
+				allowFirstLine: false,
+			},
+		}],
+		'node/no-unsupported-features': 'off',
+		'node/no-unsupported-features/es-syntax': 'off',
+		'private-props/no-use-outside': 'off',
+		'max-params': 'off',
+		'no-underscore-dangle': 'off',
+	},
+	globals: {},
+};
